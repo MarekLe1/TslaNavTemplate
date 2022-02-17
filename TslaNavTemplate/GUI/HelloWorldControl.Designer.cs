b7493875a89d5b337f3e42735d8fbe084701cde1
Marek Le <mmtrawczynski@tesla.com>
2022-02-17 12:07:33 +0100

    embedded tesla logo

--- TslaNavTemplate/GUI/HelloWorldControl.Designer.cs
+++ TslaNavTemplate/GUI/HelloWorldControl.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.helloWorldText = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // helloWorldText
@@ -43,6 +45,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.button1.Location = new System.Drawing.Point(70, 112);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 39);
@@ -51,17 +54,27 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(70, 206);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // HelloWorldControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.helloWorldText);
             this.MaximumSize = new System.Drawing.Size(250, 300);
             this.MinimumSize = new System.Drawing.Size(250, 300);
             this.Name = "HelloWorldControl";
             this.Size = new System.Drawing.Size(250, 300);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,5 +84,6 @@
 
         private System.Windows.Forms.Label helloWorldText;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
